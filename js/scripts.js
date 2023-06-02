a2bd1afc16f155dae1c8f00d0c34b044170a6cd8
Schmelzberry <jelsberry96@gmail.com>
2023-06-02 10:46:21 -0700

    develop branching logic to gather user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+window.onload = function() {
+    const form = document.querySelector("form");
+    form.onsubmit = function (event) {
+        event.preventDefault();
+
+        let languageReveal = document.getElementById("languages");
+        languageReveal.setAttribute("class", "hidden");
+        let pythonInfo = document.getElementById("pyth");
+        pythonInfo.setAttribute("class", "hidden");
+
+        const catdog = parseInt(document.querySelector("input#catdog").value);
+
+        if (catdog >= )
+    }
+}
+
 
 
 
