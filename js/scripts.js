a64f05021f57d70b38e9c8786c3b139262642320
Schmelzberry <jelsberry96@gmail.com>
2023-06-02 13:09:59 -0700

    rewrite js functions, still developing branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
-function handleSelect (event) {
-    event.preventDefault();
-    const selection = document.getElementById("animalpref").value;
-}
 
-window.addEventListener("load", function() {
-    this.document.getElementById("select-form").addEventListener("submit", handleSelect);
-});
+
+
+
 
 
 
