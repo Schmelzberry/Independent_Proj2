a2a0f395b3580a9237cfd3e5e9a351efc08081d5
Schmelzberry <jelsberry96@gmail.com>
2023-06-02 16:33:44 -0700

    added a little js, MVP still missing

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
-let h1 = document.querySelector("h1");
-h1;
-
 window.onload = function () {
     const form = document.querySelector("form");
     form.onsubmit = function(event) {
-      event.preventDefault();
-      console.log(hello)
+        document.querySelector("id#animal").innerText = "animal";
+        document.querySelector("id#color").innerText = "color";
+        document.querySelector("id#letter").innerText = "letter";
+        document.querySelector("id#band").innerText = "band";
+        document.querySelector("id#num").innerText = "num";
 
-      
+        event.preventDefault();
+        
+        const animalValue = (document.querySelector("input#animal").value);
+
+        
+
+        }
+    }
 }
-    
 
 
-  
-  }
-
-
-
+// Hello, sorry this file is so bare. I had planned to gather all 5 form inputs as strings, count the length of the string after all questions had been answered, and then assign one of the three hidden results depending on length of their answers. I clearly failed in this but wanted whomever corrects this dumpster fire to have an idea of what I wanted to do.
 
 
 
