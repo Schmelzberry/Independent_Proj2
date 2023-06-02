bb10022f968b681505d10f620c90b8891e17516d
Schmelzberry <jelsberry96@gmail.com>
2023-06-02 11:29:05 -0700

    have the requisite amount of forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,11 @@
-window.onload = function() {
-    const form = document.querySelector("form");
-    form.onsubmit = function (event) {
-        event.preventDefault();
-
-        let languageReveal = document.getElementById("languages");
-        languageReveal.setAttribute("class", "hidden");
-        let pythonInfo = document.getElementById("pyth");
-        pythonInfo.setAttribute("class", "hidden");
-
-        const catdog = parseInt(document.querySelector("input#catdog").value);
-
-        if (catdog >= )
-    }
+function handleSelect (event) {
+    event.preventDefault();
+    const selection = document.getElementById("animalpref").value;
 }
+
+window.addEventListener("load", function() {
+    this.document.getElementById("select-form").addEventListener("submit", handleSelect);
+});
 
 
 
