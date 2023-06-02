00ae876c5a83231e4c22052d728a61373bb110d6
Schmelzberry <jelsberry96@gmail.com>
2023-06-02 13:42:55 -0700

    feature form submission via js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+// User Interface Logic
+function hideResults() {
+    document.getElementById("java").setAtrribute("class", "hidden");
+    document.getElementById("pyth").setAtrribute("class", "hidden");
+    document.getElementById("ruby").setAtrribute("class", "hidden");
+    
+}
+    
+window.onload = function () {
+  document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+    hideResults();
+    const
+  }  
+    
+    }
 
 
 
